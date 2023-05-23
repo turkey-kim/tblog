db11e805151fdba2469196af603cd7d9e6e3730f
turkey-kim <rlaxmrgml@naver.com>
2023-05-23 17:50:57 +0900

    feat: Post 페이지 디자인 수정/ 보완

--- src/views/Post.tsx
+++ src/views/Post.tsx
@@ -19,18 +19,26 @@ const EditPost = () => {
 
   return (
     <Container data-color-mode="light" ref={targetRef}>
+      <Title placeholder="발행하실 글의 제목을 입력하세요"></Title>
       <div className="wmde-markdown-var"></div>
       {targetOn ? (
-        <MDEditor value={markdown} onChange={onChangeValue} height={700} />
+        <MDEditor
+          value={markdown}
+          onChange={onChangeValue}
+          height={1000}
+          style={{ width: "100%" }}
+        />
       ) : (
         <MDEditor
           value={markdown}
           onChange={onChangeValue}
-          height={700}
-          style={{ zIndex: -1 }}
+          height={1000}
+          style={{ width: "100%", zIndex: -1 }}
         />
       )}
-      <Button text="발행"></Button>
+      <ButtonContainer>
+        <Button text="발행" size="medium"></Button>
+      </ButtonContainer>
       {/* <MDEditor.Markdown source={markdown} style={{ whiteSpace: "pre-wrap" }} /> */}
     </Container>
   );
@@ -39,6 +47,28 @@ const EditPost = () => {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
+const Title = styled.input`
+  width: 99%;
+  height: 8vh;
+  margin: 3px;
+  border: none;
+  outline: none;
+  font-size: larger;
+  font-weight: 700;
+`;
+
+const ButtonContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  position: fixed;
+  bottom: 15px;
+  right: 20px;
+`;
 export default EditPost;
