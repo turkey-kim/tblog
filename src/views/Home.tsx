5cf1a9e2ae942ec9631072eed1817e2938af1358
turkey-kim <rlaxmrgml@naver.com>
2023-05-28 11:59:50 +0900

    feat: 메인페이지 db에서 받아온 글 렌더링

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,111 +1,119 @@
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import getWritings from "../api/getWritings";
 import React, { useState, useEffect } from "react";
+import WritingImage from "../components/Writing";
 
 function Home() {
-  let [content, setContent] = useState("");
-  let [arr, setArr] = useState<string[]>([]);
+  let [arr, setArr] = useState<any>([{}]);
+  let navigate = useNavigate();
 
   useEffect(() => {
-    if (!localStorage.getItem("contents")) {
-      localStorage.setItem("contents", JSON.stringify([]));
+    async function posts() {
+      const result = await getWritings("api/get_writings");
+      // console.log(result?.data);
+      setArr(result?.data);
     }
-    let x: any = localStorage.getItem("contents");
-    x = JSON.parse(x);
-    setArr(x);
+    posts();
   }, []);
-
-  const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setContent(e.target.value);
-  };
-
-  const onClickSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-    let arrClone: string[] = [...arr];
-    arrClone.push(content);
-    setArr([...arr, content]);
-    localStorage.setItem("contents", JSON.stringify(arrClone));
-    setContent("");
-  };
 
   return (
     <Container>
-      <WritingForm>
-        <WritingSpace
-          id="writingSpace"
-          placeholder=" Write anything what you want"
-          onChange={onChangeInput}
-          value={content == "" ? "" : content}
-        ></WritingSpace>
-        <SubmitButton onClick={onClickSubmit}>글발행</SubmitButton>
-      </WritingForm>
+      <ContainerHeader></ContainerHeader>
       <CardDeque>
-        {arr.map((text, i) => {
-          return (
-            <Card>
-              <h1>No.{i + 1}</h1>
-              <p>{text}</p>
-            </Card>
-          );
-        })}
+        {arr.length != 0
+          ? arr.map((element: any) => (
+              <Card
+                onClick={() => {
+                  navigate(`/writing/${element.id}`);
+                }}
+              >
+                <WritingImage></WritingImage>
+                <CardTitle>{element.title}</CardTitle>
+                <CardFooter>
+                  <CardDate>{element.date}</CardDate>
+                  <Author>by {element.author}</Author>
+                </CardFooter>
+              </Card>
+            ))
+          : null}
       </CardDeque>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-`;
-
-const WritingForm = styled.div`
-  margin-top: 30px;
-  display: flex;
-  justify-content: center;
   width: 100%;
-`;
-const WritingSpace = styled.input`
-  width: 400px;
-  height: 30px;
-  border: none;
-  border-radius: 3px;
-  background-color: white;
-  outline: none;
+  height: 100%;
+  justify-content: center;
 `;
 
-const SubmitButton = styled.button`
-  width: 80px;
-  height: 30px;
-  background-color: ${({ theme }) => theme.color.bg150};
-  border: none;
-  border-radius: 3px;
-  margin-left: 3px;
-  font-size: small;
-  font-weight: 600;
-
-  :hover {
-    background-color: ${({ theme }) => theme.color.bg200};
-  }
+const ContainerHeader = styled.div`
+  width: 100%;
+  height: 10vh;
 `;
 
 const CardDeque = styled.div`
   display: flex;
-  gap: 10px;
+  gap: 2rem;
   flex-wrap: wrap;
-  align-items: center;
   justify-content: center;
+  align-items: center;
 `;
 
-const Card = styled.div`
+interface Props {
+  onClick?: any;
+}
+
+const Card = styled.div<Props>`
   display: flex;
   flex-direction: column;
-  margin: 15px;
+
   width: 300px;
   height: 400px;
   background-color: ${({ theme }) => theme.color.white};
   border-radius: 5px;
+  justify-content: space-between;
+  cursor: pointer;
 
   :last-child {
     justify-self: flex-end;
   }
+`;
+
+const CardTitle = styled.div`
+  text-align: center;
+  font-weight: 800;
+  font-size: 1.5rem;
+  width: 100%;
+  height: 20%;
+  padding-top: 10px;
+`;
+
+const CardFooter = styled.div`
+  display: flex;
+  width: auto;
+  height: auto;
+  flex-direction: column;
+`;
+
+const CardDate = styled.div`
+  text-align: right;
+  font-size: 0.75rem;
+  width: auto;
+  height: auto;
+  color: ${({ theme }) => theme.color.dark100};
+  margin-bottom: 0.5rem;
+`;
+
+const Author = styled.div`
+  text-align: left;
+  font-weight: 800;
+  font-size: 1rem;
+  width: auto;
+  height: auto;
+  border-top: 1px solid ${({ theme }) => theme.color.bg100};
+  padding: 0.75rem;
 `;
 
 export default Home;
