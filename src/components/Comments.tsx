5f6dc3a1993011975c12cca18227604fc5cbf2b4
turkey-kim <rlaxmrgml@naver.com>
2023-07-07 03:04:24 +0900

    feat: 댓글 컴포넌트; 대댓글 UI추가

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -8,11 +8,15 @@ import deleteComment from "../api/deleteComment";
 import editComment from "../api/editComment";
 import { RootState } from "../modules";
 import { useSelector } from "react-redux";
+import ReCommentBox from "./ReCommentBox";
+import RecommentList from "./RecommentList";
 
 function Comments() {
   const { id } = useParams();
   const [comments, setComments] = useState([]);
+  const [recomments, setRecomments] = useState([]);
   let [editTarget, setEditTarget] = useState("");
+  let [openRecomment, setOpenRecomment] = useState("");
   let [text, setText] = useState("");
 
   const user = useSelector((state: RootState) => state.userProfile?.id);
@@ -20,7 +24,9 @@ function Comments() {
   useEffect(() => {
     async function showComments() {
       const result = await getComments("api/get_comments", id);
+      const result2 = await getComments("api/get_recomments", id);
       setComments(result?.data);
+      setRecomments(result2?.data);
     }
 
     showComments();
@@ -44,62 +50,94 @@ function Comments() {
     <Container>
       {comments.length != 0
         ? comments.map((comment: any) => (
-            <Comment>
-              <Header>
-                <UserImage size="small"></UserImage>
-                <div>{comment.user}</div>
-                <div>{comment.date}</div>
-              </Header>
-              <Body>
-                {editTarget === comment._id ? (
-                  <div>
-                    <form
-                      onSubmit={() => {
-                        editThis(comment._id, text);
-                      }}
-                    >
-                      <input
-                        name="content"
-                        value={text}
-                        onChange={onChange}
-                      ></input>
-                      <Button text="수정완료"></Button>
-                    </form>
+            <>
+              <Comment>
+                <Header>
+                  <UserImage size="small"></UserImage>
+                  <div>{comment.user}</div>
+                  <div>{comment.date}</div>
+                </Header>
+                <Body>
+                  {editTarget === comment._id ? (
+                    <div>
+                      <form
+                        onSubmit={() => {
+                          editThis(comment._id, text);
+                        }}
+                      >
+                        <input
+                          name="content"
+                          value={text}
+                          onChange={onChange}
+                        ></input>
+                        <Button text="수정완료"></Button>
+                      </form>
+                      <Button
+                        text="취소"
+                        onClick={() => {
+                          setEditTarget("");
+                        }}
+                      ></Button>
+                    </div>
+                  ) : (
+                    <p>{comment.content}</p>
+                  )}
+                </Body>
+                {comment.user == user ? (
+                  <Footer>
                     <Button
-                      text="취소"
+                      margin="3px"
+                      size="small"
+                      text="수정"
                       onClick={() => {
-                        setEditTarget("");
+                        setEditTarget(comment._id);
+                        setText(comment.content);
                       }}
                     ></Button>
-                  </div>
+                    <Button
+                      margin="3px"
+                      size="small"
+                      text="삭제"
+                      onClick={() => {
+                        deleteThis(comment._id);
+                      }}
+                    ></Button>
+                  </Footer>
+                ) : null}
+                {openRecomment == comment._id ? (
+                  <Button
+                    text="답글 닫기"
+                    color="light"
+                    onClick={() => {
+                      setOpenRecomment("");
+                    }}
+                  ></Button>
                 ) : (
-                  <p>{comment.content}</p>
+                  <Button
+                    text="답글 열기"
+                    onClick={() => {
+                      setOpenRecomment(comment._id);
+                    }}
+                  ></Button>
                 )}
-              </Body>
-              {comment.user == user ? (
-                <Footer>
-                  <Button
-                    margin="3px"
-                    size="small"
-                    text="수정"
-                    onClick={() => {
-                      setEditTarget(comment._id);
-                      setText(comment.content);
-                    }}
-                  ></Button>
-                  <Button
-                    margin="3px"
-                    size="small"
-                    text="삭제"
-                    onClick={() => {
-                      deleteThis(comment._id);
-                    }}
-                  ></Button>
-                </Footer>
-              ) : (
-                <Footer></Footer>
-              )}
-            </Comment>
+              </Comment>
+              {openRecomment == comment._id ? (
+                <ReComment>
+                  {recomments.map((recomment: any) =>
+                    recomment.parentId == comment._id ? (
+                      <RecommentList
+                        user={recomment.user}
+                        date={recomment.date}
+                        content={recomment.content}
+                        id={recomment._id}
+                      ></RecommentList>
+                    ) : null
+                  )}
+
+                  <ReCommentBox parentId={openRecomment}></ReCommentBox>
+                </ReComment>
+              ) : null}
+            </>
           ))
         : null}
     </Container>
@@ -125,6 +163,17 @@ const Comment = styled.div`
   background-color: ${({ theme }) => theme.color.bg200};
 `;
 
+const ReComment = styled.div`
+  display: flex;
+  margin: 1vh;
+  border-radius: 5px;
+  flex-direction: column;
+  align-items: flex-end;
+  width: 100%;
+  height: auto;
+  background-color: ${({ theme }) => theme.color.dark150};
+`;
+
 const Header = styled.div`
   display: flex;
   justify-content: flex-start;
@@ -139,6 +188,7 @@ const Body = styled.div`
   height: auto;
   text-align: left;
 `;
+
 const Footer = styled.div`
   display: flex;
   width: 100%;
